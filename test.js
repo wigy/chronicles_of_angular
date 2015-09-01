3962ac5e6c09d81af5549e60413ffa33ab9b42a7
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2015-09-01 21:34:04 +0300

    Basic handlers for A-Z keys.

--- test.js
+++ test.js
@@ -3,9 +3,14 @@ var TestApp = angular.module('TestApp', ['coa.input.keyboard',
                                          'coa.audio.player']);
 
 TestApp.controller('TestAppController', ['$scope',
+
     function($scope) {
 
+        $scope.keyPressed = '-';
+
         $scope.storeKey = function(name) {
+            d('Key pressed:', name)
+            // TODO: Hmm, does not display anything?
             $scope.keyPressed = name;
         };
     }
