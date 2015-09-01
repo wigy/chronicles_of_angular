4b2e00daab736ff6b767df5a9cae400dc660d94a
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2015-09-01 20:16:39 +0300

    Start sketching audio player with testing files.

---
+++ test.js
@@ -0,0 +1,7 @@
+
+var TestApp = angular.module('TestApp', ['coa.input.keyboard',
+                                         'coa.audio.player']);
+
+TestApp.config(['coaKeyHandler', function(coaKeyHandler){
+    d(coaPlayer)
+}]);
