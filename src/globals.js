54ada8f3082a813c8181f10376a030b7579693a1
Tommi Ronkainen <tommi.j.ronkainen@microsoft.com>
2015-11-26 19:52:00 +0200

    Docs for d().

--- src/globals.js
+++ src/globals.js
@@ -1,8 +1,17 @@
-// TODO: Proper documentation.
-
 /**
- * Dump all arguments.
+ * Dump all arguments to the console.
+ *
+ * @param arg1 {any} Any number of arguments can be given.
+ * @param ...
+ * @param argN {any}
+ * @return The last argument <i>argN</i>.
  */
-function d(arg) {
-    console.log.apply(console, arguments);
+function d(arg1, arg2, argN) {
+
+    var args = Array.prototype.slice.call(arguments);
+    if (args.length === 0) {
+        return;
+    }
+    console.log.apply(console, args);
+    return args[args.length - 1];
 }
