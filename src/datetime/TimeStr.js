1c62ecbb24c69f904cfb3b9f9a2f9bbd30d72181
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2016-02-09 18:02:23 +0200

    Some work on TimeStr.

--- src/datetime/TimeStr.js
+++ src/datetime/TimeStr.js
@@ -12,21 +12,150 @@
 
         /**
         * @ngdoc function
-        * @name coa.auth.class:User
+        * @name coa.datetime.class:TimeStr
         * @description
         *
         * A class describing a time stamp string of form <tt>HH:MM:SS</tt>.
+        * Seconds can be added and subtrackted from the time. If it goes
+        * negative, a minus sign is added as prefix.
         */
         TimeStr = function(data) {
             this.init(data);
         };
 
+        /**
+         * @ngdoc method
+         * @name now
+         * @methodOf coa.datetime.class:TimeStr
+         * @preturns {TimeStr} Current time.
+         * @description
+         *
+         * A static method to return current time, for example
+         * <pre>
+         *   var now = TimeStr.now():
+         * </pre>
+         */
+        TimeStr.now = function() {
+            var clock = new TimeStr();
+            clock.setNow();
+            return clock;
+        };
+
         TimeStr.prototype = new Data('Time', 'coa.datetime', [
-            {time: {type: TypeStr}}
-            // TODO: Add boolean flag 'negative'.
+            {time: {type: TypeStr, default: '00:00:00'}},
+            {negative: {type: TypeStr}}
+            // TODO: Change 'negative' to boolean.
         ]);
 
-        // TODO: Implement as in time2xercise.
+        TimeStr.prototype.toString = function() {
+            return this.negative ? '-' + this.time : this.time;
+        };
+
+        // TODO: Proper docs for all functions.
+        /**
+         * Value as <tt>HH:MM</tt>.
+         */
+        TimeStr.prototype.short = function() {
+            if (this.negative) {
+                return this.toString().substr(0, 6);
+            }
+            return this.toString().substr(0, 5);
+        };
+
+        /**
+         * Add to this time.
+         */
+        TimeStr.prototype.add = function(h, m, s) {
+            var parts = this.time.split(':');
+            s = s + parseInt(parts[2]);
+            m += Math.floor(s/60);
+            s = s % 60;
+            m = m + parseInt(parts[1]);
+            h += Math.floor(m/60);
+            m = m % 60;
+            h = h + parseInt(parts[0]);
+            h = h % 24;
+
+            this.time = ('0' + h).substr(-2,2) + ':' + ('0' + m).substr(-2,2) + ':' + ('0' + s).substr(-2,2);
+        };
+
+        /**
+         * Add or subtract seconds.
+         */
+        TimeStr.prototype.addSeconds = function(num) {
+            if (num > 0) {
+                this.add(0, 0, num);
+            }
+            else {
+                var zero = new Time('00:00:00');
+                zero.addSeconds(this.seconds() + num);
+                this.time = zero.time;
+            }
+        };
+
+        /**
+         * Set the time to the current time.
+         */
+        TimeStr.prototype.setNow = function() {
+            this.time = (new Date()).toJSON().substr(11, 8);
+            var hours = (-(new Date()).getTimezoneOffset() / 60);
+            if (hours > 0) {
+                this.add(hours, 0, 0);
+            }
+            if (hours < 0) {
+                this.add(24 + hours, 0, 0);
+            }
+        };
+
+        /**
+         * Check if this time is not already as much as the given time.
+         */
+        TimeStr.prototype.notYet = function(time) {
+            return this.time.toString() < time.toString();
+        };
+
+        /**
+         * Check if this time is already past the given time.
+         */
+        TimeStr.prototype.isPast = function(time) {
+            return this.time.toString() > time.toString();
+        };
+
+        /**
+         * Check if this time is at least the given time.
+         */
+        TimeStr.prototype.isAlready = function(time) {
+            return this.time.toString() >= time.toString();
+        };
+
+        /**
+         * Convert time to seconds.
+         */
+        TimeStr.prototype.seconds = function() {
+            var h, m, s;
+            var parts = this.time.split(':');
+            h = parseInt(parts[0]);
+            m = parseInt(parts[1]);
+            s = parseInt(parts[2]);
+            return h*60*60 + m*60 + s;
+        };
+
+        /**
+         * Subtract another time from this time and return new time instance.
+         */
+        TimeStr.prototype.diff = function(time) {
+            var a = this.seconds(), b = time.seconds();
+            var ret = new Time();
+
+            if (a < b) {
+                ret.add(0, 0, b-a);
+                ret.negative = true;
+            } else {
+                ret.add(0, 0, a-b);
+            }
+
+            return ret;
+        };
 
         return TimeStr;
     }]);
--- test/datetime/timestr_spec.js
+++ test/datetime/timestr_spec.js
@@ -9,9 +9,29 @@ describe('module coa.datetime, TimeStr class', function() {
         });
     });
 
-    it('can calculate seconds', function() {
-        // TODO: Test TimeStr throughly.
-        d(new TimeStr());
-        expect(true).toBe(true);
+    it('initializes values correctly', function() {
+        var zero = new TimeStr();
+        expect(zero.toString()).toBe('00:00:00');
+        expect(zero.short()).toBe('00:00');
+        expect(zero.seconds()).toBe(0);
+
+        var time = new TimeStr({time: '19:12:59'});
+        expect(time.toString()).toBe('19:12:59');
+        expect(time.short()).toBe('19:12');
+        expect(time.seconds()).toBe(69179);
+
+        // TODO: Initialize with another Time instance (needs support in Data.init()).
+        // TODO: Initialize with single string (needs support in Data.init() and definition of primary field).
     });
+
+    it('adds hours, minutes and seconds correctly', function() {
+        var time = new TimeStr({time: '15:00:00'});
+        time.addSeconds(1)
+        expect(time.toString()).toBe('15:00:01');
+        time.add(1, 1, 1);
+        expect(time.toString()).toBe('16:01:02');
+    });
+
+    // TODO: Test comparison.
+    // TODO: Support and test negative additions.
 });
